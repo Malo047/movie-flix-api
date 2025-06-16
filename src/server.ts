27e43088434e2004e9c3e548554d9d8869d18000
Marlon <marlonhoffmann001@gmail.com>
2025-06-15 21:45:44 -0300

    Reconfigurando servidor
    Fiz a reconfiguração do servidor devido a algumas inconsistencias

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./generated/prisma";
 import express from "express";
 
 
@@ -6,11 +6,33 @@ const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
-app.get("/movies", async (req, res) =>{
-    const movies = await prisma.movies.findMany();
+app.get("/movies", async (req, res) => {
+    const movies = await prisma.movies.findMany({
+        include: {
+            genre: true,
+            language: true
+        },
+        orderBy: {
+            title: "asc"
+        }
+    });
     res.json(movies);
 });
 
-app.listen(port, () =>{
+// app.post("/movies", async (_, res) => {
+//     await prisma.movies.create({
+//         data:{
+//             id: 16,
+//             title: "O atirador",
+//             release_date: "2016-07-25T12:00:00-03:00",
+//             genre_id: 1,
+//             language: "Portugues",
+//             oscar_count: 2
+//         }
+//     })
+//     res.status(201).send("Filme adicionado com sucesso")
+// })
+
+app.listen(port, () => {
     console.log(`Servidor em execução na porta ${port}`);
 });
