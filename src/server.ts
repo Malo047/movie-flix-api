808bd94e8f7647b8af928306aed11c8c9b307b46
Marlon <marlonhoffmann001@gmail.com>
2025-06-15 21:59:34 -0300

    Adicionando comentarios

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import { PrismaClient } from "./generated/prisma";
-import express from "express";
+import { PrismaClient } from "./generated/prisma"; //Ele é a maneira como faço a conexão do meu banco de dados.
+import express from "express"; //Com ele crio as rotas para manipulação.
 
 
-const port = 3000;
-const app = express();
-const prisma = new PrismaClient();
+const port = 3000; //Define minha porta de serviço.
+const app = express(); //Instacio o express para usar.
+const prisma = new PrismaClient(); //Instancio o prisma para através dele manupular meu banco de dados.
 
 app.get("/movies", async (req, res) => {
     const movies = await prisma.movies.findMany({
@@ -19,20 +19,19 @@ app.get("/movies", async (req, res) => {
     res.json(movies);
 });
 
-// app.post("/movies", async (_, res) => {
-//     await prisma.movies.create({
-//         data:{
-//             id: 16,
-//             title: "O atirador",
-//             release_date: "2016-07-25T12:00:00-03:00",
-//             genre_id: 1,
-//             language: "Portugues",
-//             oscar_count: 2
-//         }
-//     })
-//     res.status(201).send("Filme adicionado com sucesso")
-// })
-
+app.post("/movies", async (_, res) => {
+    await prisma.movies.create({
+        data:{
+            title: "Tropa de elite",
+            release_date: "2014-05-05T12:00:00.000Z",
+            genre_id: 1,
+            language_id: 2,
+            oscar_count: 2
+        }
+    })
+    res.status(201).send("Filme adicionado com sucesso")
+})
+                                                            //Aqui é para retornar no terminal quando estiver rodando meu servidor.
 app.listen(port, () => {
     console.log(`Servidor em execução na porta ${port}`);
-});
+}); 
