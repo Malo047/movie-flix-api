644da442f4f87e09e2ac1458cff6a7cbb9682728
Marlon <marlonhoffmann001@gmail.com>
2025-06-20 20:07:01 -0300

    Gerado build do projeto, porem teve algumas inconsistências no meu prisma onde precisei mudar algumas configs e gerar novamente.

--- dist/src/server.js
+++ dist/src/server.js
@@ -1,14 +1,17 @@
-import { PrismaClient } from "@prisma/client"; //Ele é a maneira como faço a conexão do meu banco de dados.
-import express from "express"; //Com ele crio as rotas para manipulação.
-import swaggerUi from "swagger-ui-express"
-import swaggerDocument from "../src/swagger.json"
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = require("@prisma/client"); //Ele é a maneira como faço a conexão do meu banco de dados.
+const express_1 = __importDefault(require("express")); //Com ele crio as rotas para manipulação.
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+const swagger_json_1 = __importDefault(require("../src/swagger.json"));
 const port = 3000; //Define minha porta de serviço.
-const app = express(); //Instacio o express para usar.
-const prisma = new PrismaClient(); //Instancio o prisma para através dele manupular meu banco de dados.
-app.use(express.json()); //Aqui serve para o express aceitar um JSON para o corpo da requisição.
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
+const app = (0, express_1.default)(); //Instacio o express para usar.
+const prisma = new client_1.PrismaClient(); //Instancio o prisma para através dele manupular meu banco de dados.
+app.use(express_1.default.json()); //Aqui serve para o express aceitar um JSON para o corpo da requisição.
+app.use('/docs', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_json_1.default));
 app.get("/movies", async (req, res) => {
     const movies = await prisma.movies.findMany({
         include: {
@@ -27,8 +30,8 @@ app.get("/movies/:genreName", async (req, res) => {
     //retornar os filmes 
     try {
         const genre = req.params.genreName;
-        const moviesFilteredByGenerName =  await prisma.movies.findMany({
-            include:{
+        const moviesFilteredByGenerName = await prisma.movies.findMany({
+            include: {
                 genre: true,
                 language: true
             },
@@ -39,62 +42,58 @@ app.get("/movies/:genreName", async (req, res) => {
                         mode: "insensitive"
                     },
                 },
-            }    
+            }
         });
         res.json(moviesFilteredByGenerName);
         res.status(200);
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({message:"Não foi possivel conectar ao servidor."})
     }
-    
-    
+    catch (error) {
+        console.log(error);
+        res.status(500).send({ message: "Não foi possivel conectar ao servidor." });
+    }
 });
 app.put("/movies/:id", async (req, res) => {
     const id = Number(req.params.id); // Transformando string que vem do req para number.
     const data = { ...req.body };
     data.release_date = data.release_date ? new Date(data.release_date) : undefined; // Convertendo String para data se a data for válida.
-
     try {
         const movie = await prisma.movies.findUnique({
             where: {
                 id: id,
             }
         });
-
         if (!movie) {
             res.status(404).send({ message: "Filme não encontrado" });
-            return
-        };
+            return;
+        }
+        ;
         await prisma.movies.update({
             where: {
                 id: id,
             },
             data: data,
         });
-        res.status(200).send({ message: "Alterado com sucesso." })
-
-    } catch (error) {
+        res.status(200).send({ message: "Alterado com sucesso." });
+    }
+    catch (error) {
         console.log(error);
         res.status(500).send({ message: "Não foi possível alterar o filme." });
-        return
+        return;
     }
 });
 app.post("/movies", async (req, res) => {
     try {
         const { title, release_date, genre_id, language_id, oscar_count } = req.body; //Aqui to desestruturando para receber os dados.
-
         const duplicatedTitle = await prisma.movies.findFirst({
             where: {
-                title: { equals: title, mode: "insensitive" },  //Aqui verifica o titulo como um ignoreCase.
+                title: { equals: title, mode: "insensitive" }, //Aqui verifica o titulo como um ignoreCase.
             }
         });
         if (duplicatedTitle) {
-            res.status(409).send({ message: "Filme já cadastrado." })
-            return
-        };
-
+            res.status(409).send({ message: "Filme já cadastrado." });
+            return;
+        }
+        ;
         await prisma.movies.create({
             data: {
                 title: title, //E aqui é como se fosse title do banco de dados recebe req.body.title que é o conteudo do JSON do body.
@@ -105,40 +104,38 @@ app.post("/movies", async (req, res) => {
             }
         });
         res.status(201).send({ message: "Filme cadastrado com sucesso." });
-    } catch (error) {
-        console.log(error)
+    }
+    catch (error) {
+        console.log(error);
         res.status(500).send({ message: "Erro ao cadastrar filme." });
     }
-
 });
 app.delete("/movies/:id", async (req, res) => {
     const id = Number(req.params.id);
-
     try {
         const movie = await prisma.movies.findUnique({
             where: {
                 id: id,
             }
         });
-
-        if(!movie){
-            res.status(404).send({message:"Filme não encontrado"});
-            return
-        };
-
+        if (!movie) {
+            res.status(404).send({ message: "Filme não encontrado" });
+            return;
+        }
+        ;
         await prisma.movies.delete({
             where: {
                 id: id,
             }
         });
-        res.status(200).send({mwssage:"Filme deletado com sucesso."});
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ message: "Erro ao deletar filme." })
+        res.status(200).send({ mwssage: "Filme deletado com sucesso." });
     }
-
+    catch (error) {
+        console.log(error);
+        res.status(500).send({ message: "Erro ao deletar filme." });
+    }
 });
 //Aqui é para retornar no terminal quando estiver rodando meu servidor.
 app.listen(port, () => {
     console.log(`Servidor em execução na porta ${port}`);
-}); 
+});
